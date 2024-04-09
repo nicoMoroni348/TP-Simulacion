185e4bf44a91e52634f39ca8fd59e251ee958c22
ianrichard <ianrichard323@gmail.com>
2024-04-09 19:30:01 -0300

    primer commit amigos

--- distribucion_exponencial.py
+++ distribucion_exponencial.py
@@ -1,13 +1,12 @@
-# -- DISTRIBUCIÓN EXPONENCIAL --
+import math
 
-# media = 4.0
-# lamb = 1/media
+# -- DISTRIBUCIÓN EXPONENCIAL --#
+def distribucion_exponencial(numeros_uniformes_0_1, media): 
+    lamb = 1/media
 
-# numeros_distribucion_exponencial_negativa = []
+    numeros_distribucion_exponencial_negativa = []
 
-# for rnd in numeros_uniformes_0_1:
-#     x = (-1/lamb) * math.log(1-rnd, e)
-#     numeros_distribucion_exponencial_negativa.append(x)
-    
-
-#(-1/lambd) * ln(1-rnd)
+    for rnd in numeros_uniformes_0_1:
+        x = (-1/lamb) * math.log(1-rnd, e)
+        numeros_distribucion_exponencial_negativa.append(x)
+    return numeros_distribucion_exponencial_negativa         
--- distribucion_uniforme.py
+++ distribucion_uniforme.py
@@ -1,15 +1,13 @@
 #-- DISTRIBUCIÓN UNIFORME --
 
-a = float(input("Ingrese el límite inferior de la distribución uniforme: "))
-b = float(input("Ingrese el límite superior de la distribución uniforme: "))
 
-x = a + rnd * (b-a) 
 
-numeros_distribucion_uniforme = []
+#x = a + rnd * (b-a) 
+
 
 if a >= b:
    print("El límite inferior a debe ser menor que el límite superior b.")
 else:
-  for rnd in numeros:
+  for rnd in numeros_uniformes_0_1:
       x = a + rnd * (b-a)
       numeros_distribucion_uniforme.append(x)
