90a914784b567d37682b70fd8d626dfe9404d01c
Mateo Ignacio Varela <matevarela32@gmail.com>
2024-06-06 22:31:47 -0300

    validaciones para la interfaz desarrolladas, no implementadas todavía

---
+++ Tp4/validaciones.py
@@ -0,0 +1,54 @@
+def validar_parametros_simulacion(i, j, x):
+
+    try:
+        i = int(i)
+        j = float(j)
+        x = float(x)
+    except ValueError:
+        return False
+    
+    if j > x or j < 0 or x < 0 or i < 1 or i > 100000:
+        return False
+
+    return i, j
+
+
+def validar_demora(demora_min, demora_max):
+
+    try:
+        demora_min = float(demora_min)
+        demora_max = float(demora_max)
+    except ValueError:
+        return False
+    
+    if demora_min < 0 or demora_max < 0 or demora_max < demora_min:
+        return False
+
+    return demora_min, demora_max
+
+
+def validar_float_positivo(value):
+
+    try:
+        value = float(value)
+    except ValueError:
+        return False
+    
+    if value < 0:
+        return False
+
+    return value
+
+
+def validar_media_desviacion(media, desviacion):
+
+    try:
+        media = float(media)
+        desviacion = float(desviacion)
+    except ValueError:
+        return False
+    
+    if media < 0 or desviacion < 0 or desviacion > media:
+        return False
+
+    return media, desviacion
